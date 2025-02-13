0cc0a1fb21406ef828da3e55ce782c47503acc73
nhatduy111003 <phamnhatduy08@gmail.com>
2025-02-13 21:59:26 +0700

    Header+Body+Footer BetaBeta

--- src/views/home/header.view.tsx
+++ src/views/home/header.view.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Header: React.FC = () => {
   return (
-    <div>header</div>
+    <header className="header">
+    <img src="/src/assets/images/auth/logo-zendo.png" alt="NovaSix Logo" className="logo-auth "/>
+    <nav className="nav">
+        <a href="#" className="nav-link">Guideline</a>
+        <a href="#" className="nav-link">About Us</a>
+        <a href="#" className="nav-link">FAQS</a>
+        <a href="#" className="nav-link">Policies & Terms</a>
+    </nav>
+    <div className="auth-links">
+    <button className="join-button">Login</button>
+        <button className="join-button">Join Us Now</button>
+    </div>
+</header>
   )
 }
 
