b2d0cb99c887ce98d0567b8ca2e972c508bf9fa0
nhatduy111003 <phamnhatduy08@gmail.com>
2025-02-15 21:34:49 +0700

    Setup DashBoard + Fix HomePage Repeat

--- src/layouts/home.layout.tsx
+++ src/layouts/home.layout.tsx
@@ -1,6 +1,5 @@
 import Header from '@/views/home/header.view'
 import Footer from '@/views/home/footer.view'
-import Body from '@/views/home/body.view'
 import Bg from '@/assets/images/unauth/bg-novasix-home.png'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
@@ -9,7 +8,6 @@ const home: React.FC = () => {
   return (
     <div className="home-layout" style={{ backgroundImage: `url(${Bg})` }}>
         <Header />
-        <Body />
         <Outlet/>
         <Footer />
     </div>
