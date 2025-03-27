6a54d66099c3cd37e02cc1588300ac0712710329
Duchai232 <Duchai757@gmail.com>
2025-03-27 15:28:20 +0700

    loading login

--- src/views/auth/login.view.tsx
+++ src/views/auth/login.view.tsx
@@ -8,6 +8,7 @@ import { IFormDataLogin } from "@/types/auth.interface";
 import { AuthService } from "@/services/auth.service";
 import { handleError } from "@/utils/catch-error";
 import { setLocalStorage } from "@/utils/localstorage";
+import Loader from "@/components/loading";
 const Login = () => {
   const navigate = useNavigate();
   const [formDataLogin, setFormDataLogin] = useState<IFormDataLogin>({
@@ -40,94 +41,98 @@ const Login = () => {
     }
   };
   return (
-    <Spin spinning={isLoading}>
-      <div className="auth-container">
-        <img src={logo} />
-        <h1 className="title-primary">LOG IN</h1>
-        <Row className="full-width" gutter={[12, 12]}>
-          <Col span={24}>
-            <MButton
-              title="Login Google"
-              icon="FaGoogle"
-              style={{ width: "100%" }}
-            />
-          </Col>
-          <Col span={24}>
-            <MButton
-              title="Login Facebook"
-              icon="FaFacebook"
-              style={{ width: "100%" }}
-            />
-          </Col>
-        </Row>
-        <Row
-          className="full-width"
-          gutter={[12, 12]}
-          style={{ marginTop: "24px" }}
-        >
-          <Col span={24} className="full-width">
-            <MInput
-              title="Username"
-              placeholder="Enter your username"
-              onChange={(e) =>
-                setFormDataLogin((prev) => ({
-                  ...prev,
-                  username: e.target.value,
-                }))
-              }
-            />
-          </Col>
-          <Col span={24} className="full-width">
-            <MInput
-              title="Password"
-              type="password"
-              placeholder="Enter your password"
-              onChange={(e) =>
-                setFormDataLogin((prev) => ({
-                  ...prev,
-                  password: e.target.value,
-                }))
-              }
-            />
-          </Col>
-        </Row>
-        <Row
-          className="full-width"
-          gutter={[12, 12]}
-          style={{ marginTop: "24px" }}
-        >
-          <Col span={24}>
-            <MButton
-              title="Login"
-              style={{ width: "100%" }}
-              type="fill"
-              onClick={() => handleLogin()}
-            />
-          </Col>
-        </Row>
-        <Row
-          className="full-width footer-auth"
-          gutter={[12, 12]}
-          style={{ marginTop: "24px" }}
-        >
-          <Col span={24}>
-            <span className="forgot-password">Forgot Password?</span>
-          </Col>
-          <Col span={24}>
-            <span>
-              You don't have an account yet,{" "}
-              <strong
-                className="navigate"
-                onClick={() => navigate("/register")}
-              >
-                {" "}
-                register now.
-              </strong>
-            </span>
-          </Col>
-        </Row>
-      </div>
-    </Spin>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="auth-container">
+          <img src={logo} />
+          <h1 className="title-primary">LOG IN</h1>
+          <Row className="full-width" gutter={[12, 12]}>
+            <Col span={24}>
+              <MButton
+                title="Login Google"
+                icon="FaGoogle"
+                style={{ width: "100%" }}
+              />
+            </Col>
+            <Col span={24}>
+              <MButton
+                title="Login Facebook"
+                icon="FaFacebook"
+                style={{ width: "100%" }}
+              />
+            </Col>
+          </Row>
+          <Row
+            className="full-width"
+            gutter={[12, 12]}
+            style={{ marginTop: "24px" }}
+          >
+            <Col span={24} className="full-width">
+              <MInput
+                title="Username"
+                placeholder="Enter your username"
+                onChange={(e) =>
+                  setFormDataLogin((prev) => ({
+                    ...prev,
+                    username: e.target.value,
+                  }))
+                }
+              />
+            </Col>
+            <Col span={24} className="full-width">
+              <MInput
+                title="Password"
+                type="password"
+                placeholder="Enter your password"
+                onChange={(e) =>
+                  setFormDataLogin((prev) => ({
+                    ...prev,
+                    password: e.target.value,
+                  }))
+                }
+              />
+            </Col>
+          </Row>
+          <Row
+            className="full-width"
+            gutter={[12, 12]}
+            style={{ marginTop: "24px" }}
+          >
+            <Col span={24}>
+              <MButton
+                title="Login"
+                style={{ width: "100%" }}
+                type="fill"
+                onClick={() => handleLogin()}
+              />
+            </Col>
+          </Row>
+          <Row
+            className="full-width footer-auth"
+            gutter={[12, 12]}
+            style={{ marginTop: "24px" }}
+          >
+            <Col span={24}>
+              <span className="forgot-password">Forgot Password?</span>
+            </Col>
+            <Col span={24}>
+              <span>
+                You don't have an account yet,{" "}
+                <strong
+                  className="navigate"
+                  onClick={() => navigate("/register")}
+                >
+                  {" "}
+                  register now.
+                </strong>
+              </span>
+            </Col>
+          </Row>
+        </div>
+      )}
+    </>
   );
 };
 
