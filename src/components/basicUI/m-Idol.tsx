6fd40b739e64b20f5cbc14cad0f55b4ae3ef4b7c
ShuMoo789 <shuvuive789@gmail.com>
2025-03-30 01:17:40 +0700

    fix daily to see weather

---
+++ src/components/basicUI/m-Idol.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MIdol: React.FC = () => {
+  return (
+    <div className="e-card playing">
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+    </div>
+  )
+}
+
+export default MIdol
